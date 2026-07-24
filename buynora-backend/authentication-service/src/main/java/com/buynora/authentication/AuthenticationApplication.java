bdf667d7fefc6ae3a09b70be3e055514127b1376
Om Maurya <ommaurya2580@gmail.com>
2026-07-24 22:14:24 +0530

    fix(auth): exclude common-library SecurityConfig from ComponentScan

--- buynora-backend/authentication-service/src/main/java/com/buynora/authentication/AuthenticationApplication.java
+++ buynora-backend/authentication-service/src/main/java/com/buynora/authentication/AuthenticationApplication.java
@@ -2,9 +2,22 @@ package com.buynora.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
-@SpringBootApplication(scanBasePackages = {"com.buynora.authentication"})
+@SpringBootApplication(
+    scanBasePackages = {"com.buynora.authentication"},
+    exclude = { SecurityAutoConfiguration.class }
+)
+@ComponentScan(
+    basePackages = {"com.buynora.authentication"},
+    excludeFilters = @ComponentScan.Filter(
+        type = FilterType.REGEX,
+        pattern = "com\\.buynora\\.common\\.config\\..*"
+    )
+)
 @EnableDiscoveryClient
 public class AuthenticationApplication {
     public static void main(String[] args) {
